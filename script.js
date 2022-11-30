991e4aed1e9996fb3b8a72297440baa2c2a7b46a
CEscripts <farazsharif2021@gmail.com>
2022-11-30 11:45:18 -0500

    finilized the project, added JS code and footer

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
+const imageOne = document.querySelector(".image-1")
+const imageTwo = document.querySelector(".image-2")
+const imageThree = document.querySelector(".image-3")
 function changeImage1(){
-    if(document.getElementsByClassName('image-1').src = './resources/HTML.JPG'){
-        document.getElementsByClassName('image-1').src = './resources/HTML-mouseover.JPG'
-        console.log('check')
+    if(imageOne.src = './resources/HTML.JPG'){
+        imageOne.src = './resources/HTML-mouseover.JPG'
+    }
+}
+function changeImage2(){
+    if(imageTwo.src = './resources/css.JPG'){
+        imageTwo.src = './resources/CSS-mouseover.JPG'
     }
 }
 
-document.getElementsByClassName('image-1').onclick = changeImage1()
+function resetImage1 () {
+     if (imageOne.src = './resources/HTML-mouseover.JPG') {
+        imageOne.src = './resources/HTML.JPG'
+    }
+
+}
+function resetImage2 () {
+    if (imageTwo.src = './resources/CSS-mouseover.JPG') {
+        imageTwo.src = './resources/css.JPG'
+   }
+
+}
+
+
+imageOne.onmouseover = changeImage1
+imageOne.onmouseout = resetImage1
+imageTwo.onmouseover = changeImage2
+imageTwo.onmouseout = resetImage2
+
