9aa6f3498b747d76727bbbf693c67f6a1cf14803
CEscripts <farazsharif2021@gmail.com>
2023-07-24 10:30:02 -0400

    major changes to css as well as links to social media was added

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-const imageOne = document.querySelector(".image-1")
 const imageTwo = document.querySelector(".image-2")
 const imageThree = document.querySelector(".image-3")
-function changeImage1(){
-    if(imageOne.src = './resources/HTML.JPG'){
-        imageOne.src = './resources/HTML-mouseover.JPG'
-    }
-}
-function changeImage2(){
-    if(imageTwo.src = './resources/css.JPG'){
-        imageTwo.src = './resources/CSS-mouseover.JPG'
+const btnEl = document.querySelector(".joinBut")
+
+function changeImage2() {
+    if (imageTwo.src = './resources/css.JPG') {
+        imageTwo.style.opacity = '0';
+        setTimeout(() => {
+            imageTwo.src = './resources/CSS-mouseover.JPG';
+            imageTwo.style.opacity = '0.8';
+        }, 300); // Delay the image source change for 300 milliseconds to allow the opacity transition to take effect
     }
 }
 
-function resetImage1 () {
-     if (imageOne.src = './resources/HTML-mouseover.JPG') {
+function resetImage1() {
+    if (imageOne.src = './resources/HTML-mouseover.JPG') {
         imageOne.src = './resources/HTML.JPG'
     }
 
 }
-function resetImage2 () {
+function resetImage2() {
     if (imageTwo.src = './resources/CSS-mouseover.JPG') {
         imageTwo.src = './resources/css.JPG'
-   }
+    }
 
 }
 
 
-imageOne.onmouseover = changeImage1
-imageOne.onmouseout = resetImage1
 imageTwo.onmouseover = changeImage2
 imageTwo.onmouseout = resetImage2
+const click = async () => {
 
+}
+btnEl.addEventListener('click', click);
+console.log(fetch('./index3.html'))
